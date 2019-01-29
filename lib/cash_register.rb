ac866caf73efc935dbae79b6b0202fc14af66863
Jamal Gelin <gelin.jamal@gmail.com>
2019-01-29 07:21:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,9 +2,9 @@ class CashRegister
   attr_accessor :items, :discount, :total, :last_transction
 
   def intialize(discount=nil)
-    @total=0
-    @discount=discount
-    @items =[]
+    self.total=0
+    self.discount=discount
+    self.items =[]
   end
 
   def add_item(title, amount, quantity=1)
